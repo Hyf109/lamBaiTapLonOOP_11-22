5fdfb6d0a6d6316e1f8d6172625ef8c00625119d
Nguyễn Thành Huy <122582591+Hyf109@users.noreply.github.com>
2023-12-05 13:26:56 +0700

    update

--- src/main/java/com/example/demo/GoogleTranslate.java
+++ src/main/java/com/example/demo/GoogleTranslate.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -13,23 +14,34 @@ public class GoogleTranslate {
 
     // API translate
     public static String translate(String text) throws IOException {
-        String urlStr = "https://script.google.com/macros/s/AKfycbxueKM8M8EMiiQYoYEmTZZR3L-TQ61S65DVeNvUo5DaxWbFUgw5/exec" +
-                "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8) +
-                "&target=" + VIE_LANG +
-                "&source=" + ENG_LANG;
 
-        // Use the URI constructor to create a URL
-        URI uri = URI.create(urlStr);
-        StringBuilder response = new StringBuilder();
-        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        String urlStr = "https://mymemory.translated.net/en/English/Vietnamese/" +
+                "?q=" + URLEncoder.encode(text, StandardCharsets.UTF_8).replace("&", "%26") +
+                "&target=" + URLEncoder.encode(VIE_LANG, StandardCharsets.UTF_8).replace("&", "%26") +
+                "&source=" + URLEncoder.encode(ENG_LANG, StandardCharsets.UTF_8).replace("&", "%26");
+
+        String responseBody = "";
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            responseBody = response.toString();
+
         }
-        in.close();
-        return response.toString();
+        conn.disconnect();
+        //System.out.println(responseBody);
+        return responseBody;
     }
-}
 
+}
