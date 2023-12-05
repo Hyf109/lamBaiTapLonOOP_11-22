eaff0d3721796577c3a0ba64df56266fbf49af74
Nguyễn Thành Huy <122582591+Hyf109@users.noreply.github.com>
2023-12-05 12:01:03 +0700

    new Update

--- src/main/java/com/example/demo/Start.java
+++ src/main/java/com/example/demo/Start.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 public class Start {
     public static void main(String[] args){
+        System.setProperty("freetts.voices","com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
         DictionaryApp.main(args);
     }
 }
